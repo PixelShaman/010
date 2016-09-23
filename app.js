64a95997d038c9a7466ec798c3e9b57d416308e6
PixelShaman <nicholaslawrence@Nicholass-MBP-2.lan>
2016-09-23 21:03:56 +0100

    ert

---
+++ app.js
@@ -0,0 +1,15 @@
+var http = require('http');
+const PORT=8080; 
+
+
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url + '<br/><br/><br/>');
+}
+
+var server = http.createServer(handleRequest);
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+
+});
+
